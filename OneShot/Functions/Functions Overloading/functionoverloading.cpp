0b44f0f22cf6aaa76772b38c6c3d6de5f358da15
Aditya Choudhary <developmentwithaditya@gmail.com>
2024-06-19 08:38:29 +0530

    Update functionoverloading.cpp
    fixing the bug.

--- OneShot/Functions/Functions Overloading/functionoverloading.cpp
+++ OneShot/Functions/Functions Overloading/functionoverloading.cpp
@@ -14,8 +14,10 @@ int main()
     If we call add() with two parameter it will retuen a+b Or  If we call the same add() with three parameter it will retuen a+b+c
     */
 
-    int add_Of_Two = add(2, 4);      // This is first version of maximum() with the paramenter of 2 integers.
     int add_Of_Three = add(2, 4, 8); // This is Second version of maximum() with the paramenter of 3 integers.
+    int add_Of_Two = add(2, 4);      // This is first version of add() with the paramenter of 2 integers.
+
+    int add_Of_Three = add(2, 4, 8); // This is Second version of add() with the paramenter of 3 integers.
 
     cout << "Addition of two numbers is: " << add_Of_Two << endl;
     cout << "Addition of three numbers is: " << add_Of_Three << endl;
