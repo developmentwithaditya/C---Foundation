a018883fdfae59891dc14f8be307bb9b66b2d9fc
Aditya Choudhary <developmentwithaditya@gmail.com>
2024-06-19 08:40:20 +0530

    Update functionoverloading.cpp

--- OneShot/Functions/Functions Overloading/functionoverloading.cpp
+++ OneShot/Functions/Functions Overloading/functionoverloading.cpp
@@ -14,7 +14,6 @@ int main()
     If we call add() with two parameter it will retuen a+b Or  If we call the same add() with three parameter it will retuen a+b+c
     */
 
-    int add_Of_Three = add(2, 4, 8); // This is Second version of maximum() with the paramenter of 3 integers.
     int add_Of_Two = add(2, 4);      // This is first version of add() with the paramenter of 2 integers.
 
     int add_Of_Three = add(2, 4, 8); // This is Second version of add() with the paramenter of 3 integers.
